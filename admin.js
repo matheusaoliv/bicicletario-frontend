cc60352df889436a4d61d681710b2b6a59d4f6bd
matheusaoliv <107213643+matheusaoliv@users.noreply.github.com>
2025-09-01 00:18:19 -0300

    Add files via upload

--- admin.js
+++ admin.js
@@ -42,7 +42,7 @@ adminLoginForm.addEventListener('submit', async (e) => {
     const res = await fetch(`${API_BASE_URL}/api/auth/login`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ login, senha })
+      body: JSON.stringify({ nome_usuario: login, senha })
     });
     if (!res.ok) {
       const errorData = await res.json().catch(() => ({}));
